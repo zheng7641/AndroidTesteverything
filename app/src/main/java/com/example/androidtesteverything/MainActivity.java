e82d6d7c0a8b41fc56aa1c0b972d18c8e878a948
zheng7641 <zheng7641@163.com>
2023-07-15 19:23:50 +0800

    1

---
+++ app/src/main/java/com/example/androidtesteverything/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.androidtesteverything;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        TextView tv = findViewById(R.id.tv);
+        tv.setText("你好");
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setClass(MainActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+
+}

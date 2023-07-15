a4a6ecc1ff6a71cea35a98f8d707e7226c5166ad
zheng7641 <zheng7641@163.com>
2023-07-16 00:06:30 +0800

    相对布局

--- app/src/main/java/com/example/androidtesteverything/MainActivity.java
+++ app/src/main/java/com/example/androidtesteverything/MainActivity.java
@@ -21,7 +21,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent();
-                intent.setClass(MainActivity.this, MainActivity.class);
+                intent.setClass(MainActivity.this, MainActivity3.class);
                 startActivity(intent);
             }
         });

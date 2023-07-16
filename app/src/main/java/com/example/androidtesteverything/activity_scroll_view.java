7b4049b471579a3ef3e6a83a2f71af70db9d974c
zheng7641 <zheng7641@163.com>
2023-07-16 21:57:51 +0800

    ccrollview

---
+++ app/src/main/java/com/example/androidtesteverything/activity_scroll_view.java
@@ -0,0 +1,14 @@
+package com.example.androidtesteverything;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class activity_scroll_view extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scroll_view);
+    }
+}

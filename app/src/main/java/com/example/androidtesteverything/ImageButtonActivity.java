59dc61abe13f0c39996783074d9da596bf1e2d98
zheng7641 <zheng7641@163.com>
2023-07-17 23:59:33 +0800

    imagebutton

---
+++ app/src/main/java/com/example/androidtesteverything/ImageButtonActivity.java
@@ -0,0 +1,14 @@
+package com.example.androidtesteverything;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ImageButtonActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_image_button);
+    }
+}
